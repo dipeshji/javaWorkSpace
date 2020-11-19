6635d389fc53c408eb79001265b494ec27731d9d
Dipesh Patidar <dpatidar336@gmail.com>
2020-11-19 13:49:51 +0530

    added critera methods

--- HibernateWorkSpace/HibernateQueryLanguage/src/hQL/HqlMain.java
+++ HibernateWorkSpace/HibernateQueryLanguage/src/hQL/HqlMain.java
@@ -2,10 +2,13 @@ package hQL;
 
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import java.util.List;
 import java.util.Map;
@@ -24,15 +27,26 @@ public class HqlMain extends Throwable{
 		System.out.println("main started");
 		
 		/*selectAllHql();
-		selectAllSql();*/
+		selectAllSql();
+		selectAllCriteria();*/
 //		getSomeColumnSql();
 //		getSomeColumnHql();
+//		getSomeColumnCriteria();
 //		orderBySql();
 //		orderByHql();
+//		OrderByCriteria();
 //		deleteSql();
 //		deleteHql();
+//		deleteCriteria(2410);
+//		updateCriteria(1625);
 //		updateSql();
-		updateHql();
+//		updateHql();
+//		selectTopSql();
+//		selectTopHql();
+//		selectTopCriteria();
+//		countSql();
+//		countHql();
+		countCriteria();
 		
 	}
 	
@@ -80,6 +94,25 @@ public class HqlMain extends Throwable{
 		session.close();
 	}
 	
+//	==================================================
+	
+	public static void selectAllCriteria() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		Criteria crit = session.createCriteria(PartDTO.class);
+		List res = crit.list();
+		Iterator itr = res.iterator();
+		
+		PartDTO emp;
+		while(itr.hasNext()) {
+			emp = (PartDTO) itr.next();
+			System.out.println(emp.getFirstName() + " " + emp.getLastName());
+		}
+		
+		session.close();
+	}
+	
 //	===============================================
 	
 	public static void getSomeColumnSql() {
@@ -98,9 +131,9 @@ public class HqlMain extends Throwable{
 		session.close();
 	}
 	
+//	==================================================
 	
-//	===============================================	
-	public static void getSomeColumnHql() {
+	public static void someColumnHql() {
 		Session session = startSession();
 		session.beginTransaction();
 		
@@ -120,6 +153,31 @@ public class HqlMain extends Throwable{
 			
 			System.out.println(fName = " " + lName);
 			
+		}
+		session.close();
+		
+	}
+	
+//	===============================================
+	
+	public static void getSomeColumnCriteria() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("output from getSomeColumnCriteria");
+		
+		Criteria crit = session.createCriteria(PartDTO.class);
+		ProjectionList p = Projections.projectionList();
+		p.add(Projections.property("firstName"));
+		p.add(Projections.property("lastName"));
+		crit.setProjection(p);
+		List res = crit.list();
+		Iterator itr = res.iterator();
+		
+		Object columns[];
+		while(itr.hasNext()) {
+			columns = (Object[])itr.next();
+			System.out.println(columns[0] + " " + columns[1]);
 		}
 		session.close();
 		
@@ -163,6 +221,33 @@ public class HqlMain extends Throwable{
 	
 //	==================================================
 	
+	public static void OrderByCriteria() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("output from OrderByCriteria");
+		
+		Criteria crit = session.createCriteria(PartDTO.class);
+		ProjectionList p = Projections.projectionList();
+		p.add(Projections.property("firstName"));
+		p.add(Projections.property("jobTitle"));
+		crit.addOrder(Order.asc("jobTitle"));
+		crit.addOrder(Order.asc("firstName"));
+		
+		List res = crit.setProjection(p).list();
+		Iterator itr = res.iterator();
+		Object columns[];
+		while(itr.hasNext()) {
+			columns = (Object[])itr.next();
+			System.out.println(columns[0] + " | " + columns[1]);
+		}
+		
+		session.close();
+		
+	}
+	
+//	==================================================
+	
 	public static void deleteSql() {
 		Session session = startSession();
 		session.beginTransaction();
@@ -185,6 +270,23 @@ public class HqlMain extends Throwable{
 		qr.setParameter("name", "Dipesh");
 		int res = qr.executeUpdate();
 		session.getTransaction().commit();
+		session.close();
+		
+	}
+	
+//	===========================================
+	
+	public static void deleteCriteria(int employeeId) {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		PartDTO emp = (PartDTO)session.createCriteria(PartDTO.class).add(Restrictions.eq("employeeNumber",employeeId)).uniqueResult();
+		
+		session.delete(emp);
+		session.getTransaction().commit();
+		
+		System.out.println("employee " + emp.getFirstName() + " with id " + emp.getEmployeeNumber() + " is deleted");
+		
 		session.close();
 		
 	}
@@ -221,6 +323,146 @@ public class HqlMain extends Throwable{
 		System.out.println(res);
 		session.getTransaction().commit();
 		session.close();
+	}
+	
+//	===========================================
+	
+	public static void updateCriteria(int employeeId) {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		PartDTO emp = (PartDTO)session.createCriteria(PartDTO.class).add(Restrictions.eq("employeeNumber", employeeId)).uniqueResult();
+		emp.setJobTitle("Exc Manager");
+		
+		session.update(emp);
+		session.getTransaction().commit();
+		session.close();
+		System.out.println("employee " + emp.getFirstName() + " with id " + emp.getEmployeeNumber() + " has been updated");
+	}
+	
+//	===========================================
+	public static void selectTopSql() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		Query qr = session.createNativeQuery("select firstName from employees limit 10").setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		List res = qr.list();
+		
+		for(Object obj: res) {
+			Map row = (Map) obj;
+			System.out.println(row.get("firstName"));
+		}
+		session.close();
+	}
+	
+//	===========================================
+	public static void selectTopHql() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		List res = session.createQuery("select firstName from PartDTO").setFirstResult(1).setMaxResults(10).getResultList();
+		Iterator itr = res.iterator();
+		
+				
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		session.close();
+	}
+	
+//	============================================
+	
+	public static void selectTopCriteria() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("You are viewing output from selectTopCriteria");
+		Criteria crit = session.createCriteria(PartDTO.class);
+		
+		List res = crit.setFirstResult(5).setMaxResults(10).list();
+		
+		Iterator itr = res.iterator();
+		
+		PartDTO emp;
+		int i = 1;
+		while(itr.hasNext()) {
+			emp = (PartDTO)itr.next();
+			System.out.println(i + ") " + emp.getFirstName() + " " + emp.getLastName());
+			i++;
+		}
+		
+		session.close();
+	}
+	
+//	==========================================
+	
+	public static void countSql() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("you are viewing output from countSql");
+		
+		Query q = session.createNativeQuery("select jobTitle, count(*) as c from employees group by jobTitle").setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		
+//		System.out.println(q.getSingleResult());
+		
+		List res = q.list();
+		Iterator itr = res.iterator();
+		
+		Object columns[];
+		while(itr.hasNext()) {
+			Map row = (Map) itr.next();
+			System.out.println(row.get("jobTitle") + " | " + row.get("c"));
+		}
+		
+		session.close();
+	}
+	
+//	============================================
+	
+	public static void countHql() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("this is from countHql");
+		
+		List res = session.createQuery("select lastName, count(*) as c from PartDTO group by lastname").list();
+		
+		Iterator itr = res.iterator();
+		Object columns[];
+		while(itr.hasNext()) {
+			columns = (Object[]) itr.next();
+			System.out.println(columns[0] + " | " + columns[1]);
+		}
+		
+		session.close();
+		
+	}
+	
+//	============================================
+	public static void countCriteria() {
+		Session session = startSession();
+		session.beginTransaction();
+		
+		System.out.println("this is from countCriteria");
+		
+		Criteria crit = session.createCriteria(PartDTO.class);
+		ProjectionList p = Projections.projectionList();
+		
+		p.add(Projections.property("reportsTo"));
+		p.add(Projections.count("reportsTo"));
+		p.add(Projections.groupProperty("reportsTo"));
+		
+		List res = crit.setProjection(p).list();
+		Iterator itr = res.iterator();
+		Object columns[];
+		
+		while(itr.hasNext()) {
+			columns = (Object[]) itr.next();
+			System.out.println(columns[0] + " | " + columns[1]);
+		}
+		session.close();
+		
 	}
 	
 }
---
+++ ServletWorkspace/ContextandConfig/WebContent/WEB-INF/classes/ctxandcfg/Register.java
@@ -0,0 +1,46 @@
+package ctxandcfg;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UseContext2
+ */
+//@WebServlet("/register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext ctx = getServletContext();
+		// TODO Auto-generated method stub
+		
+		response.getWriter().append("ServletContxt-2 [ My name is ").append(ctx.getInitParameter("Name"));
+		response.getWriter().append(", and i use ").append(ctx.getInitParameter("Mobile"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
